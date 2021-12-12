62411ce5e8ac6391d745b46f2bd5a3b95e47a419
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-12-12 01:13:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+  int dim;
+  cin >> dim;
+  float num[dim];
+  for (int i=0; i<dim; i++){
+    cin >> num[i];
+  }
+  MaxArray(num,dim);
+  cout << MaxArray(num,dim) <<endl;
   return 0;
 }
